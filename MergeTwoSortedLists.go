68c324a55f10f923f49afdf4ece29303c0ba4647
Claratorres1414 <claratorres12345679@gmail.com>
2026-06-12 17:49:32 -0300

    MergeTwoSortedLists: corrige nome de var para nó inicial head->tail

--- MergeTwoSortedLists.go
+++ MergeTwoSortedLists.go
@@ -2,25 +2,25 @@ package main
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	result := &ListNode{}
-	head := result
+	tail := result
 
 	for list1 != nil || list2 != nil {
 		if list1 == nil {
-			head.Next = list2
+			tail.Next = list2
 			return result.Next
 		}
 		if list2 == nil {
-			head.Next = list1
+			tail.Next = list1
 			return result.Next
 		}
 		if list1.Val <= list2.Val {
-			head.Next = &ListNode{Val: list1.Val}
+			tail.Next = &ListNode{Val: list1.Val}
 			list1 = list1.Next
 		} else {
-			head.Next = &ListNode{Val: list2.Val}
+			tail.Next = &ListNode{Val: list2.Val}
 			list2 = list2.Next
 		}
-		head = head.Next
+		tail = tail.Next
 	}
 	return result.Next
 }
