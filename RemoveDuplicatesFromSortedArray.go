56e2ca9340e7c01b5d67d3edc31ad40a80dd9f26
Claratorres1414 <claratorres12345679@gmail.com>
2026-06-13 07:36:35 -0300

    RemoveDuplicatesFromSortedArray: otimiza uso de recursos de memória no código

--- RemoveDuplicatesFromSortedArray.go
+++ RemoveDuplicatesFromSortedArray.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func removeDuplicates(nums []int) int {
-	var res []int
-	val := nums[0]
-	res = append(res, val)
 	count := 1
 	for i := 1; i < len(nums); i++ {
-		if val < nums[i] {
+		if nums[i-1] < nums[i] {
+			nums[count] = nums[i]
 			count++
-			res = append(res, nums[i])
 		}
-		val = nums[i]
-	}
-	for i := 0; i < len(res); i++ {
-		nums[i] = res[i]
 	}
 	return count
 }
