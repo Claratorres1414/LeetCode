f3fc64ad7c3dc1fa52a04e542c2b8eab40fc02b6
Claratorres1414 <claratorres12345679@gmail.com>
2026-06-16 14:02:48 -0300

    SearchInsertPosition: reorganiza solução em formato comum de busca binária

--- SearchInsertPosition.go
+++ SearchInsertPosition.go
@@ -3,27 +3,19 @@ package main
 import "fmt"
 
 func searchInsert(nums []int, target int) int {
-	for i := len(nums) / 2; i > 0 && i < len(nums); {
-		if nums[i] == target {
-			return i
-		} else if target < nums[i] {
-			if i-1 >= 0 && nums[i-1] < target {
-				return i
-			}
-			i = i / 2
-		} else if target > nums[i] {
-			if i+1 < len(nums) && target < nums[i+1] {
-				return i + 1
-			}
-			i++
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		} else if target < nums[mid] {
+			r = mid - 1
+		} else if target > nums[mid] {
+			l = mid + 1
 		}
 	}
 
-	if nums[len(nums)-1] < target {
-		return len(nums)
-	}
-
-	return 0
+	return l
 }
 
 func main() {
