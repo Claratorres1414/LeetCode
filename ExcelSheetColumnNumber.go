6d5a7ee74231f2660d08639976cf7ffaf77238aa
Claratorres1414 <claratorres12345679@gmail.com>
2026-07-21 07:18:02 -0300

    ExcelSheetColumnNumber: melhor solução alcançada para o problema utilizando potenciação

---
+++ ExcelSheetColumnNumber.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var alfToNum = map[string]int{
+	"A": 1,
+	"B": 2,
+	"C": 3,
+	"D": 4,
+	"E": 5,
+	"F": 6,
+	"G": 7,
+	"H": 8,
+	"I": 9,
+	"J": 10,
+	"K": 11,
+	"L": 12,
+	"M": 13,
+	"N": 14,
+	"O": 15,
+	"P": 16,
+	"Q": 17,
+	"R": 18,
+	"S": 19,
+	"T": 20,
+	"U": 21,
+	"V": 22,
+	"W": 23,
+	"X": 24,
+	"Y": 25,
+	"Z": 26,
+}
+
+func titleToNumber(columnTitle string) int {
+	result := 0
+	count := 0
+	mult := 1
+
+	for i := len(columnTitle) - 1; i >= 0; i-- {
+		if i > 0 {
+			mult = pow(alfToNum[string(columnTitle[count])], i)
+			result += mult * 26
+		} else {
+			result += alfToNum[string(columnTitle[count])]
+		}
+
+		count++
+	}
+
+	return result
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func main() {
+	fmt.Println(titleToNumber("FXSHRXW"))
+}
